cb4964cbe4c049b9f91ff2556dc0040acd026709
Sujit <sujitkarki703@gmail.com>
2024-12-05 16:51:31 +0545

    feat: add setter and getter method of cogUrl value on store.js

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,10 @@
 export class Store {
   private static _gcpData: string[][] = [];
+  private static _cogUrl: string = '';
 
   //   event for data update
   static readonly GCP_DATA_UPDATE = 'gcp-data-update';
+  static readonly COG_URL_UPDATE = 'cog-url-update';
 
   //   Methods to access and update the data
   static getGcpData(): string[][] {
@@ -12,5 +14,14 @@ export class Store {
   static setGcpData(data: string[][]) {
     this._gcpData = data;
     document.dispatchEvent(new CustomEvent(Store.GCP_DATA_UPDATE, { detail: data }));
+  }
+
+  static getCogUrl(): string {
+    return this._cogUrl;
+  }
+
+  static setCogUrl(data: string) {
+    this._cogUrl = data;
+    document.dispatchEvent(new CustomEvent(Store.COG_URL_UPDATE, { detail: data }));
   }
 }
