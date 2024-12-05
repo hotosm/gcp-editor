2c74e526e0eef1ace7567e519bb25327aa18b19d
Sujit <sujitkarki703@gmail.com>
2024-12-05 17:28:59 +0545

    feat: add setter and getter method of projection value on store.js fire event on value update

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,12 @@
 export class Store {
   private static _gcpData: string[][] = [];
   private static _cogUrl: string = '';
+  private static _projection: string = '';
 
   //   event for data update
   static readonly GCP_DATA_UPDATE = 'gcp-data-update';
   static readonly COG_URL_UPDATE = 'cog-url-update';
+  static readonly PROJECTION_UPDATE = 'cog-url-update';
 
   //   Methods to access and update the data
   static getGcpData(): string[][] {
@@ -23,5 +25,14 @@ export class Store {
   static setCogUrl(data: string) {
     this._cogUrl = data;
     document.dispatchEvent(new CustomEvent(Store.COG_URL_UPDATE, { detail: data }));
+  }
+
+  static getProjection(): string {
+    return this._projection;
+  }
+
+  static setProjection(data: string) {
+    this._projection = data;
+    document.dispatchEvent(new CustomEvent(Store.PROJECTION_UPDATE, { detail: data }));
   }
 }
