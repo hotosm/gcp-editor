de8b84b230b26aded550b37feebf6cd4d7adead8
Sujit <suzeett10@gmail.com>
2024-12-24 17:08:52 +0545

    feat: update data input UI

--- src/gcp-editor-wrapper.ts
+++ src/gcp-editor-wrapper.ts
@@ -7,10 +7,10 @@ import '@hotosm/ui/dist/hotosm-ui';
 import './components/GcpDataInput/index';
 import './components/GcpMarking/index';
 import './components/GcpResult/index';
-import { get } from 'ol/proj';
 
 @customElement('gcp-editor')
 export class GcpEditor extends LitElement {
+  @property({ type: String }) rawImageUrl = '';
   @property({ type: Number }) activeStep = 1;
   @property() gcpData = null;
   @property() setGcpDataWithXY = {};
@@ -22,14 +22,17 @@ export class GcpEditor extends LitElement {
 
   connectedCallback() {
     super.connectedCallback();
+    Store.setRawImageUrl(this.rawImageUrl);
     // Listen for updates to CSV data
     document.addEventListener(Store.GCP_DATA_UPDATE, this.handleGcpDataUpdate.bind(this));
     document.addEventListener(Store.GCP_DATA_WITH_IMAGE_XY_UPDATE, this.handleGcpDataWithXYUpdate.bind(this));
+    document.addEventListener(Store.ACTIVE_STEP_UPDATE, this.handleActiveStepUpdate.bind(this));
   }
 
   disconnectedCallback() {
     document.removeEventListener(Store.GCP_DATA_UPDATE, this.handleGcpDataUpdate.bind(this));
     document.removeEventListener(Store.GCP_DATA_WITH_IMAGE_XY_UPDATE, this.handleGcpDataWithXYUpdate.bind(this));
+    document.removeEventListener(Store.ACTIVE_STEP_UPDATE, this.handleActiveStepUpdate.bind(this));
     super.disconnectedCallback();
   }
 
@@ -43,21 +46,15 @@ export class GcpEditor extends LitElement {
     this.setGcpDataWithXY = CustomEvent?.detail;
   }
 
-  private handleClickBack() {
-    this.activeStep = this.activeStep - 1;
-  }
-
-  private handleClickNext() {
-    const projection = Store.getProjection();
-    if (this.activeStep === 1 && (!this.gcpData || !get(projection))) return;
-    if (this.activeStep === 2 && !Object.keys(this.setGcpDataWithXY || {}).length) return;
-    this.activeStep = this.activeStep + 1;
+  handleActiveStepUpdate(event: Event) {
+    const CustomEvent = event as CustomEvent<any>;
+    this.activeStep = CustomEvent.detail;
   }
 
   render() {
     return html`
-      <div class="tw-h-full tw-w-full tw-pb-28">
-        <div class="tw-px-20 tw-py-10 tw-h-full tw-w-full tw-border-b">
+      <div class="tw-h-full tw-w-full tw-bg-gray-100">
+        <div class="tw-px-20 tw-py-10 tw-h-full tw-w-full">
           ${this.activeStep === 1
             ? html`
                 <gcp-data-input></gcp-data-input>
@@ -69,17 +66,6 @@ export class GcpEditor extends LitElement {
             : html`
                 <gcp-result></gcp-result>
               `}
-        </div>
-
-        <div class="tw-flex tw-justify-between tw-w-full tw-fixed tw-bottom-10 tw-px-20">
-          ${this.activeStep > 1
-            ? html`
-                <hot-button @click=${this.handleClickBack}>Back</hot-button>
-              `
-            : html`
-                <div></div>
-              `}
-          <hot-button @click=${this.handleClickNext}>Next</hot-button>
         </div>
       </div>
     `;
