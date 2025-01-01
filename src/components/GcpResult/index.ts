9c9384d4cd1e89c8c033ff3cab00c24388624a40
Sujit <suzeett10@gmail.com>
2025-01-01 14:26:08 +0545

    feat: add callback function and final button text

--- src/components/GcpResult/index.ts
+++ src/components/GcpResult/index.ts
@@ -1,5 +1,5 @@
 import { css, html, LitElement, PropertyValues } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement, property, state } from 'lit/decorators.js';
 import { Store } from '../../store';
 
 // Define the type for a row in the gcp.txt file
@@ -25,6 +25,8 @@ export class GcpResult extends LitElement {
    * Holds the raw GCP data fetched from the store.
    */
   @property() gcpList = Store.getGcpDataWithXY();
+  @state() buttonText = Store.getFinalButtonText();
+  @state() finalButtonClickFunction = Store.getCallbackFunc();
 
   /**
    * Property: gcpInCsv
@@ -115,6 +117,8 @@ export class GcpResult extends LitElement {
    */
   protected firstUpdated(_changedProperties: PropertyValues): void {
     this.gcpInCsv = this.convertToArray(this.gcpList);
+    this.buttonText = Store.getFinalButtonText();
+    this.finalButtonClickFunction = Store.getCallbackFunc();
   }
 
   /**
@@ -187,6 +191,27 @@ export class GcpResult extends LitElement {
     Store.setActiveStep(2);
   }
 
+  private handleFinalButtonClick() {
+    if (this.finalButtonClickFunction) {
+      if (!this.gcpInCsv || this.gcpInCsv.length <= 1) return;
+      // Header for the projection (hardcoded for now)
+      // TODO support other coord systems / not hardcoded to EPSG:4326
+      const header = '+proj=utm +zone=10 +ellps=WGS84 +datum=WGS84 +units=m +no_defs\n';
+      // Convert GCP data to space-separated rows
+      const rows = this.gcpInCsv
+        .slice(1) // Skip headers
+        .map((row) => `${row[0]} ${row[1]} ${row[2]} ${row[3]} ${row[4]} ${row[5]}`) // Format: X Y Z ImageX ImageY FileName
+        .join('\n');
+
+      const finalContent = header + rows;
+      // Create a Blob for the file and trigger download
+      const blob = new Blob([finalContent], { type: 'text/plain;charset=utf-8;' });
+      this.finalButtonClickFunction(blob);
+    } else {
+      this.handleGcpFileDownload();
+    }
+  }
+
   render() {
     return html`
       <div class="table-wrapper">
@@ -222,7 +247,7 @@ export class GcpResult extends LitElement {
       </div>
       <div class="button-wrapper">
         <hot-button @click=${this.handlePreviousClick}>Previous</hot-button>
-        <hot-button @click=${this.handleGcpFileDownload}>Finish</hot-button>
+        <hot-button @click=${this.handleFinalButtonClick}>${this.buttonText}</hot-button>
       </div>
     `;
   }
